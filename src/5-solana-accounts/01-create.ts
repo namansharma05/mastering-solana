2422810c7aba3424c2897c0927ecc46108cc14c9
namansharma05 <namanks0511@gmail.com>
2026-02-25 23:50:11 +0530

    revision create account 0.1

--- src/5-solana-accounts/01-create.ts
+++ src/5-solana-accounts/01-create.ts
@@ -1,19 +1,9 @@
 import {
-  appendTransactionMessageInstruction,
   createSolanaRpc,
   createSolanaRpcSubscriptions,
-  createTransactionMessage,
   generateKeyPairSigner,
-  getProgramDerivedAddress,
-  getSignatureFromTransaction,
-  isOffCurveAddress,
-  isSolanaError,
-  pipe,
-  sendAndConfirmTransactionFactory,
-  setTransactionMessageFeePayerSigner,
-  setTransactionMessageLifetimeUsingBlockhash,
-  signTransactionMessageWithSigners,
-  SOLANA_ERROR__BLOCKHASH_STRING_LENGTH_OUT_OF_RANGE,
+  getMinimumBalanceForRentExemption,
+  lamports,
 } from "@solana/kit";
 import { config } from "../config";
 import { loadSignerFromFile } from "../wallet";
@@ -22,59 +12,23 @@ import {
   SYSTEM_PROGRAM_ADDRESS,
 } from "@solana-program/system";
 
+// connection to cluster
 const rpc = createSolanaRpc(config.RPC_HTTP);
 const rpcSubscriptions = createSolanaRpcSubscriptions(config.RPC_WS);
 
-const wallet = await loadSignerFromFile();
-console.log(`wallet address: ${wallet.address}`)
-
-const { value: latestBlockhash } = await rpc.getLatestBlockhash().send();
-
-const space = 0n;
-const rentInLamports = await rpc
-  .getMinimumBalanceForRentExemption(space)
-  .send();
-console.log(
-  `Minimum balance for rent exempt for ${space} bytes: ${Number(rentInLamports) / Number(config.LAMPORTS_PER_SOL)} lamports.`,
-);
+// new solana account payer
+const wallet = await loadSignerFromFile(); // 29KKX9fQspSenNUibR9fxJCLvwGfozFPGbt486SF8JqY
 
 const newAccount = await generateKeyPairSigner();
+const space = 0n;
+const rentLamports = BigInt(getMinimumBalanceForRentExemption(space));
+
 const createAccountInstruction = getCreateAccountInstruction({
   payer: wallet,
   newAccount: newAccount,
-  lamports: rentInLamports,
   space: space,
+  lamports: rentLamports,
   programAddress: SYSTEM_PROGRAM_ADDRESS,
 });
 
-const seeds = ["solana"];
-const [pda, bump] = await getProgramDerivedAddress({
-    programAddress: SYSTEM_PROGRAM_ADDRESS,
-    seeds: seeds,
-})
-console.log(`PDA Off Curve: ${isOffCurveAddress(pda)}`);
-
-const transactionMessage = pipe(
-  createTransactionMessage({ version: 0 }),
-  (tx) => setTransactionMessageFeePayerSigner(wallet, tx),
-  (tx) => setTransactionMessageLifetimeUsingBlockhash(latestBlockhash, tx),
-  (tx) => appendTransactionMessageInstruction(createAccountInstruction, tx),
-);
-
-const signedTransaction = await signTransactionMessageWithSigners(transactionMessage);
-
-const transactionToSend = {...signedTransaction, lifetimeConstraint: latestBlockhash}
-
-try {
-    await sendAndConfirmTransactionFactory({rpc, rpcSubscriptions})(transactionToSend, {commitment: "confirmed"});
-} catch (error) {
-    if(isSolanaError(error, SOLANA_ERROR__BLOCKHASH_STRING_LENGTH_OUT_OF_RANGE)) {
-        console.log(`blockhash expired`);
-        throw error;
-    } else {
-        throw error;
-    }
-}   
-
-const transactionSignature = getSignatureFromTransaction(signedTransaction);
-console.log(`Transaction Signature for create account: ${transactionSignature}`);
+console.log(`create account instructoin: `, createAccountInstruction)
