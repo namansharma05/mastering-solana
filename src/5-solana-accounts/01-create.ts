40e8cb9fe690cab393fc507cda4689c669dbc065
namansharma05 <namanks0511@gmail.com>
2026-02-26 08:06:18 +0530

    rivision of solana-account creation

--- src/5-solana-accounts/01-create.ts
+++ src/5-solana-accounts/01-create.ts
@@ -1,9 +1,15 @@
 import {
+  appendTransactionMessageInstructions,
   createSolanaRpc,
   createSolanaRpcSubscriptions,
+  createTransactionMessage,
   generateKeyPairSigner,
   getMinimumBalanceForRentExemption,
-  lamports,
+  pipe,
+  sendAndConfirmTransactionFactory,
+  setTransactionMessageFeePayerSigner,
+  setTransactionMessageLifetimeUsingBlockhash,
+  signTransactionMessageWithSigners,
 } from "@solana/kit";
 import { config } from "../config";
 import { loadSignerFromFile } from "../wallet";
@@ -19,16 +25,45 @@ const rpcSubscriptions = createSolanaRpcSubscriptions(config.RPC_WS);
 // new solana account payer
 const wallet = await loadSignerFromFile(); // 29KKX9fQspSenNUibR9fxJCLvwGfozFPGbt486SF8JqY
 
-const newAccount = await generateKeyPairSigner();
-const space = 0n;
-const rentLamports = BigInt(getMinimumBalanceForRentExemption(space));
+// latest blockhash
+const { value: latestBlockhash } = await rpc.getLatestBlockhash().send();
 
+// space in bytes for new account
+const space = 0n;
+// minimum balance the account needs to live on-chain
+const rentInLamports = BigInt(getMinimumBalanceForRentExemption(space));
+
+// generating new private and public key
+const newAccount = await generateKeyPairSigner();
+
+// create new account instruction
 const createAccountInstruction = getCreateAccountInstruction({
   payer: wallet,
   newAccount: newAccount,
   space: space,
-  lamports: rentLamports,
+  lamports: rentInLamports,
   programAddress: SYSTEM_PROGRAM_ADDRESS,
 });
 
-console.log(`create account instructoin: `, createAccountInstruction)
+// create new account transaction message
+const transactionMessage = pipe(
+  createTransactionMessage({ version: 0 }),
+  (tx) => setTransactionMessageFeePayerSigner(wallet, tx),
+  (tx) => setTransactionMessageLifetimeUsingBlockhash(latestBlockhash, tx),
+  (tx) => appendTransactionMessageInstructions([createAccountInstruction], tx),
+);
+
+// signing the transaction
+const signedTransaction =
+  await signTransactionMessageWithSigners(transactionMessage);
+
+try {
+  // sending and confirming transaction
+  await sendAndConfirmTransactionFactory({ rpc, rpcSubscriptions })(
+    { ...signedTransaction, lifetimeConstraint: latestBlockhash },
+    { commitment: "confirmed" },
+  );
+} catch (e) {
+  // catching the error
+  throw e;
+}
